34e03ae7dba766ce147bb74d8611affd70ee6665
Trond Patrick Wurschmidt <trond2003@gmail.com>
2021-11-10 18:25:53 +0100

    Fixed some bugs and made better tests

--- junit5-jupiter-starter-maven/src/main/java/com/example/project/LeapYear.java
+++ junit5-jupiter-starter-maven/src/main/java/com/example/project/LeapYear.java
@@ -3,9 +3,13 @@ package com.example.project;
 public class LeapYear {
     public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
-            if (year % 400 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
                 return false;
-            } else return year % 100 != 0;
+            }
+            return true;
         }
         return false;
     }
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/LeapYearTests.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/LeapYearTests.java
@@ -1,19 +1,37 @@
 package com.example.project;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-class LeapYearTests {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-    @Test
-    public void Find_Leap_year() {
-        assertEquals(true, LeapYear.isLeapYear(4));
-        assertEquals(true, LeapYear.isLeapYear(1236));
-        assertEquals(true, LeapYear.isLeapYear(1864));
-        assertEquals(true, LeapYear.isLeapYear(2384));
-        assertEquals(true, LeapYear.isLeapYear(404));
-        assertEquals(false, LeapYear.isLeapYear(400));
-        assertEquals(false, LeapYear.isLeapYear(3333));
-        assertEquals(false, LeapYear.isLeapYear(1700));
+public class LeapYearTests {
+    @Nested
+    public class Year_is_leap_year {
+        @ParameterizedTest
+        @ValueSource(ints = {2016, 2012, 1976, 4})
+        public void when_divisible_by_4_but_not_100(int year) {
+            assertTrue(LeapYear.isLeapYear(year));
+        }
+        @ParameterizedTest
+        @ValueSource(ints = {2000, 1600, 400})
+        public void when_divisible_by_400(int year) {
+            assertTrue(LeapYear.isLeapYear(year));
+        }
+    }
+    @Nested
+    public class Year_is_not_leap_year {
+        @ParameterizedTest
+        @ValueSource(ints = {2017, 2013, 2001, 1989, 3})
+        public void when_not_divisible_by_4(int year) {
+            assertFalse(LeapYear.isLeapYear(year));
+        }
+        @ParameterizedTest
+        @ValueSource(ints = {1900, 1800, 1700})
+        public void when_divisible_by_100_but_not_400(int year) {
+            assertFalse(LeapYear.isLeapYear(year));
+        }
     }
 }
